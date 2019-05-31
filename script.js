055603bb299c2c5cf2c20506db1e0db01880c0dc
n0y0x <44902757+n0y0x@users.noreply.github.com>
2019-05-31 15:22:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let Logo = document.querySelector(".SmallTitle");
+
+window.addEventListener("scroll", function () {
+  if(window.pageYOffset > 160) {
+    Logo.style.height = ("5rem")
+  } else {
+    Logo.style.height = ("0")
+  }
+})
+
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
